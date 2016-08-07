1b7ffd583f23ce4278c37c198f78173abb50ae11
Arthelon <hsing.daniel@gmail.com>
2016-08-07 11:44:05 +0800

    ADded calendar, style changes

--- controllers/home.js
+++ controllers/home.js
@@ -11,7 +11,25 @@ exports.index = function(req, res) {
     console.log(universities)
     res.render('home', {
       title: 'Home',
-      universities
+      universities,
+      prash: arr
     });
   })
 };
+
+
+var arr = [
+  {
+    title: "HKUST is outstanding at arts",
+    date: "2015-07-12",
+    author: "Peter John"
+  }, {
+    title: "Harvard is awesome",
+    date: "2014-03-02",
+    author: "Daniel John"
+  },
+  {
+    title: "No school fees?",
+    date: "2016-02-07",
+    author: "Mary Jane"
+  }]

b2c4cc7d2b9104f8da0e7314a58e0f806c98e51b
prashanth.chandra <prashanth@lalamove.com>
2016-08-06 23:16:24 -0400

    ADD: unis on home page

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,17 @@
 /**
  * GET /
  */
+const models = require('../models/')
+
 exports.index = function(req, res) {
-  res.render('home', {
-    title: 'Home'
-  });
+  models.University.find({}, null, {limit: 4})
+  .populate('majors')
+  .exec()
+  .then(universities => {
+    console.log(universities)
+    res.render('home', {
+      title: 'Home',
+      universities
+    });
+  })
 };
